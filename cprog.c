fcd250cead796f4d1f20030b463f0f3c5a27eacf
gtruecode <ravikumarpal175@gmail.com>
2021-05-15 13:55:19 +0530

    basic C programs

---
+++ cprog.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+ int main()  
+  {
+     printf("Name   : ravi kumar\n"); 
+     printf("DOB    : July 14, 1996\n"); 
+     printf("Mobile : 99-9999999999\n"); 
+     return(0); 
+  }
